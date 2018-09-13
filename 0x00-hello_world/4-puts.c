4189ebf15deab9402482b27d839344137fa9d647
seleniadelgado <angiedelgado08@gmail.com>
2018-09-13 00:36:37 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -7,7 +7,6 @@
 
 int main(void)
 {
- puts ("Programming is like building a multilingual puzzle\n")
-   return (0);
-
+  puts ("Programming is like building a multilingual puzzle\n")
+  return (0);
 }
