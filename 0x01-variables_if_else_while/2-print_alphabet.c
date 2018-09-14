96efdbc896a4f143a9f882977ff63cd74f71e626
seleniadelgado <angiedelgado08@gmail.com>
2018-09-14 01:34:28 +0000

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char az;
+
+	az = 'a';
+	while (az <= 'z')
+	{
+		putchar(az);
+		az = az + 1;
+	}
+	putchar('\n');
+	return (0);
+}
