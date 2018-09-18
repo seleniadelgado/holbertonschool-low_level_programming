9f398f13b0c60d451fbf8245ec5f9bb6a86edd56
seleniadelgado <angiedelgado08@gmail.com>
2018-09-18 21:14:36 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _abs(int) - display the absolute value of an integer.
+ * @x: parameter for function
+ * Return: Always 0.
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+       	x = x * -1;
+	return (x);
+}
