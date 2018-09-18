ed88f7a5c0ec9a404de9b07d01872d76e3e417cf
seleniadelgado <angiedelgado08@gmail.com>
2018-09-18 16:56:25 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,26 @@
 #include "holberton.h"
 
 /**
- * Print Alphabet - check the code for Holberton School students.
- *
- * Return: Always 0 (Success)
- */
+* print_alphabet_x10 - Write a function that prints 10 times the alphabet,
+* in lowercase, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
 void print_alphabet_x10(void)
 {
 	int b;
 	char az;
 
-	b = 0;
+	b = 1;
 	while (b <= 10)
 	{
 	az = 'a';
 	while (az <= 'z')
-		{
-			_putchar (az);
-		}
-	az++;
+	{
+		_putchar (az);
+		az++;
 	}
+	b++;
 	_putchar('\n');
+	}
 }
