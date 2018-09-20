cfef4df5df81ad2dabae883c71a0d8340807364d
seleniadelgado <angiedelgado08@gmail.com>
2018-09-20 00:29:00 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - Write a function that adds
+ * two integers and returns the result.
+ * @a: integer for parameter.
+ * @b: integer for parameter.
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
