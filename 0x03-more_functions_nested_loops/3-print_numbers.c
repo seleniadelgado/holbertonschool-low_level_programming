a61439f45fc482bc52beeb56161d2282b45620c6
seleniadelgado <angiedelgado08@gmail.com>
2018-09-20 19:48:41 +0000

    3-print_numbers.c

--- 0x03-more_functions_nested_loops/3-print_numbers.c
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,12 @@
  */
 void print_numbers(void)
 {
-	int c;
-	c = '0';
-		while (c <= '9')
-		{
-			_putchar(c);
-			c++;
-		}
+	int c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
 	_putchar('\n');
 }
