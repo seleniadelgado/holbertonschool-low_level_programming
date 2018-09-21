1a05cbc0d145a459f0cd1f5731f5be0edabfde60
seleniadelgado <angiedelgado08@gmail.com>
2018-09-21 21:47:51 +0000

    10-print_triangle.c

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -1,13 +1,25 @@
 #include "holberton.h"
 
-
 /**
- * main - check the code for Holberton School students.
+ * print_triangle - Make a triangle.
  *
  * Return: Always 0.
  */
-print_triangle(int size)
+void print_triangle(int size)
 {
+	int tri;
+	int angle;
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (tri = 1; tri <= size; tri++)
+		{
+		for (angle = size - tri; angle <= size; angle--)
+			_putchar(' ');
+		}
+		for (angle = 1; angle <= size; angle++)
+			_putchar('#');
 
-
+	}
 }
