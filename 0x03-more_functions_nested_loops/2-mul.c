36f2ef29999c5f51d9238bfa9a294a837480b00c
seleniadelgado <angiedelgado08@gmail.com>
2018-09-21 00:05:11 +0000

    2-mul.c

--- 0x03-more_functions_nested_loops/2-mul.c
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 /**
  * mul - Write a function that multiplies two integers.
- *
+ * @a: parameter used.
+ * @b: parameter used.
  * Return: Always 0.
  */
 int mul(int a, int b)
