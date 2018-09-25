abd3e3271978479df9019f1ffff5fa7f9122a813
seleniadelgado <angiedelgado08@gmail.com>
2018-09-25 20:14:22 +0000

    4-print_rev

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -2,25 +2,23 @@
 
 /**
  * print_rev - print a string in reverse, followed by a new line.
- *
+ * @s: parameter used.
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
 	int l = 0;
-	int c;
 
 	while (*s != '\0')
-        {
-                l++;
-                s++;
-        }
-
-	c = 0;
-	while (s[c] >= '\0')
 	{
-		_putchar(s[c]);
-		c++;
+		l++;
+		s++;
+	}
+	while  (l > 0)
+	{
+		s--;
+		l--;
+		_putchar(*s);
 	}
 	_putchar('\n');
 }
