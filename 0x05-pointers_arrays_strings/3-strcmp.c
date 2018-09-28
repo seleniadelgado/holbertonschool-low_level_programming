2449001cd5d213edae7a56b4fafab4463a1881dc
seleniadelgado <angiedelgado08@gmail.com>
2018-09-28 01:44:27 +0000

    3-strcmp.c

---
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - write a function that compares two strings.
+ * @s1: parameter used.
+ * @s2: parameter used.
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+	while (s1[a] != '\0' && s2[b] != '\0')
+	{
+		if (s1[a] != s2[b])
+			return (s1[a] - s2[b]);
+	}
+		a++;
+		b++;
+	}
+	return (0);
+}
