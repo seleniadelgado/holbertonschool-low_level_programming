faa579c8e830a5a6605d92484d8a31fb9fb7e5ca
seleniadelgado <angiedelgado08@gmail.com>
2018-09-28 05:16:58 +0000

    5-string_toupper.c

---
+++ 0x05-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper
+ * @char: parameter used.
+ * Return: Always 0.
+ */
+char *string_toupper(char *)
+{
+	char *a;
+
+	a = 0;
+	while(a < '\0')
+	a++;
+	return (0);
+}
