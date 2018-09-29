91fe6640e2f677e224ded9daec7d022f643de083
seleniadelgado <angiedelgado08@gmail.com>
2018-09-29 06:21:50 +0000

    8-rot13.c

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * *rot13 - Write a function that encodes a string using rot13.
+ * @s: parameter used. 
+ * Return: s.
+ */
+char *rot13(char *s)
+{
+	int a;
+
+	a = 0;
+	while (a != '\0')
+		a++;
+	return(s);
+}
