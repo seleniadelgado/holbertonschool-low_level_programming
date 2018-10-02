2b246172cecca707f6fac243e72dc376b24fd797
seleniadelgado <angiedelgado08@gmail.com>
2018-10-02 01:59:13 +0000

    0-memset.c

---
+++ 0x06-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+q#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_memset - Write a function that fills memory with a constant byte.
+ * @s: pointer.
+ * @n: parameter used for first bytes of memory area.
+ * Return: s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	a = 0;
+	while (n--)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
