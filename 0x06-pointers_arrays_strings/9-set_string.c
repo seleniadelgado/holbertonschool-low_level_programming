60bb9afdaf71cd8cb0aaafa1fb8d3bc01d4db436
seleniadelgado <angiedelgado08@gmail.com>
2018-10-02 05:48:20 +0000

    9-set_string.c

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * a function that sets the value of a pointer to a char.
+ * @s: parameter used.
+ * @to: parameter used.
+ * Return: Always 0.
+ */
+void set_string(char **s, char *to)
+{
+	s[0] = to;
+}
