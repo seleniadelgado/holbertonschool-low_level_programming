f64d0776b3daaca6cb92c1b6373f825c44274d97
seleniadelgado <angiedelgado08@gmail.com>
2018-10-02 06:40:50 +0000

    5-strstr.c

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+
+/**
+ * void print_chessboard(char (*a)[8])
+ * @a: parameter used.
+ *
+ *
+ **/
+void print_chessboard(char (*a)[8])
---
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * Write a function that prints the sum of the two diagonals of a square matrix
+ * of integers.
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+
+}
