f1650b8805264db11b22aa50925a434523cc7f6e
seleniadelgado <angiedelgado08@gmail.com>
2018-10-03 03:28:16 +0000

    7-print_chessboard.c

--- 0x06-pointers_arrays_strings/7-print_chessboard.c
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -1,9 +1,24 @@
 #include "holberton.h"
 
 /**
- * void print_chessboard(char (*a)[8])
+ * print_chessboard - function that prints a chessboard
  * @a: parameter used.
- *
  *
  **/
 void print_chessboard(char (*a)[8])
+{
+	int row = 0;
+	int column;
+
+	while (row < 8)
+	{
+		column = 0;
+		while (column < 8)
+		{
+			_putchar(a[row][column]);
+			column++;
+		}
+	row++;
+	_putchar('\n');
+	}
+}
