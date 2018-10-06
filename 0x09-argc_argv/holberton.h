87d6fb1d22c93efabf6d0d21a20614cd4603d4ef
seleniadelgado <angiedelgado08@gmail.com>
2018-10-06 23:42:07 +0000

    holberton.h

--- 0x09-argc_argv/holberton.h
+++ 0x09-argc_argv/holberton.h
@@ -1,4 +1,5 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-int _whatsmyname(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, __attribute__((unused)) char *argv[]);
 #endif
