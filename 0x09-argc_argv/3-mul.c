0aba2970b80d231454a2a465d38adaea0c07ca9d
seleniadelgado <angiedelgado08@gmail.com>
2018-10-07 01:27:34 +0000

    3-mul.c

---
+++ 0x09-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - function that multiplies two numbers.
+ * Return: 0.
+ * @argv: parameter used.
+ * @argc: parameter used.
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int result;
+	int num2;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+			return (1);
+	}
+	else
+	{
+
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+	}
+	return (0);
+}
