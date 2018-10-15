43cabd0fa48f60bd04d3308b116ed0f242516b80
seleniadelgado <angiedelgado08@gmail.com>
2018-10-15 21:30:56 +0000

    2-main.c

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program prints name of the file it was compiled from.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
