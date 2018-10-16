7cfd940ac8fee498b59cacb33d48a7c52338ac7a
seleniadelgado <angiedelgado08@gmail.com>
2018-10-16 00:00:57 +0000

    2-print_dog.c

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -12,6 +12,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)");
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
 	else
 		printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
