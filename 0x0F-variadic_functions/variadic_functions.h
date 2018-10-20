ab39a7abfa0b6ffda8f6397db57cd93eb2460358
seleniadelgado <angiedelgado08@gmail.com>
2018-10-20 06:05:41 +0000

    variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -4,4 +4,8 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char_function (char, ...);
+void print_int_function (int, ...);
+void print_float_function(float, ...);
+void print_string_function (char *, ...);
 #endif
