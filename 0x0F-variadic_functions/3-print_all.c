c8bbb8a61ef821749361dae48a8190954f6d3c28
seleniadelgado <angiedelgado08@gmail.com>
2018-10-21 03:58:40 +0000

    3-print_all.c

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -3,78 +3,91 @@
 #include <stdarg.h>
 
 /**
- * print_all - function that prints anything.
- * @format: list of types of arguments passed to the function.
- * Return: Always 0.
- * struct_print - structure.
- */
-void print_all(const char * const format, ...)
-{
-	struct_print [] = {
-		{"c", print_char_function},
-		{"i", print_int_function},
-		{"f", print_float_function},
-		{"s", print_string_function},
-		{NULL, NULL}
-};
-	unsigned int i, j;
-	va_list ap;
-
-	va_start(ap, format);
-
-	if (format == NULL)
-		printf("(nil)");
-	while ((format[i] != NULL) && (format[1] != '\0'))
-
-	i++;
-	return (print);
-}
-___________________________________________
-/**
  * print_char_function - prints char.
+ * @c: parameter passed.
  *
  *
- * @x: parameter type char passed in function.
  */
-void print_char_function(char x, ...)
+void print_char_function(va_list c)
 {
-	printf("%c", va_arg(ap, char);
+	printf("%c", va_arg(c, int));
 }
-_____________________________________________
+
 /**
  * print_int_function - prints integer.
+ * @i: paramter passed.
  *
  *
- * @a: parameter type int passed in function.
  */
-void print_int_function(int a, ...)
+void print_int_function(va_list i)
 {
-	printf("%d", va_arg(ap, int);
+	printf("%d", va_arg(i, int));
 }
-______________________________________________
+
 /**
  * print_float_function - prints a float.
+ * @f: parameter passed.
  *
  *
- * @b: parameter type float passed in function.
  */
-void print_float_function(float b, ...)
+void print_float_function(va_list f)
 {
-	printf("%f", va_arg(ap, b);
+	printf("%f", va_arg(f, double));
 }
-_______________________________________________
+
 /**
  * print_string_function - prints a string.
+ * @r: parameter passed.
  *
  *
- * @s: character string used in function.
  */
-void print_string_function(char *s, ...)
+void print_string_function(va_list r)
 {
 	char *s;
 
-	s = (va_arg(ap, char *)
+	s = (va_arg(r, char *));
 	if (s == NULL)
 		printf("(nil)");
 	printf("%s", s);
+}
+
+/**
+ * print_all - function that prints anything.
+ * @format: list of types of arguments passed to the function.
+ * Return: Always 0.
+ * print_t - structure.
+ */
+void print_all(const char * const format, ...)
+{
+	print_t names[] = {
+	{"c", print_char_function},
+	{"i", print_int_function},
+	{"f", print_float_function},
+	{"s", print_string_function},
+	{NULL, NULL}
+	};
+	va_list ap;
+	int i, j;
+	char *separator;
+
+	va_start(ap, format);
+	separator = "";
+	i = 0;
+	while (format[i] != '\0')
+	{
+		j = 0;
+		while (names[j].name != NULL)
+		{
+			if (format[i] == *(names[j].name))
+			{
+				printf("%s", separator);
+				names[j].type(ap);
+				separator = ", ";
+			}
+	j++;
+		}
+	i++;
+	}
+	printf("\n");
+	va_end(ap);
 }
