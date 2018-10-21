3b1188376b03a7412abf3979023a553bcb180a86
seleniadelgado <angiedelgado08@gmail.com>
2018-10-21 01:06:25 +0000

    updated parameters

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -6,8 +6,8 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
  * struct print - structure used for project 3.
- *
- *
+ * @name: parameter used for char.
+ * @type: parameter used for function.
  */
 
 
