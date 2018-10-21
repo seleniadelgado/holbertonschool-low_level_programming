61cf3dd613ef8b6828123b1c7634adb8c8a310e5
seleniadelgado <angiedelgado08@gmail.com>
2018-10-21 04:01:23 +0000

    variadic_functions.h

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,19 +1,27 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 /**
- * struct print - structure used for project 3.
+ * struct name - structure used for project 3.
  * @name: parameter used for char.
  * @type: parameter used for function.
  */
-
-
-typedef struct print
+typedef struct name
 {
 	char *name;
-	int (*type)(int a, int b);
+	void (*type)(va_list ap);
 } print_t;
+void print_char_function(va_list ap);
+void print_int_function(va_list ap);
+void print_float_function(va_list ap);
+void print_string_function(va_list ap);
 #endif
