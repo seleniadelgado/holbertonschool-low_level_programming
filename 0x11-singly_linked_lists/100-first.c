67d8873f50eac8ec28c8c3a71f88c8356319539a
seleniadelgado <angiedelgado08@gmail.com>
2018-10-26 21:16:36 +0000

    100-first.c

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * myStartupFun - functio that prints before main function is executed. 
+ * myStartupFun - functio that prints before main function is executed.
  */
 void myStartupFun(void) __attribute__((constructor));
 void myStartupFun(void)
