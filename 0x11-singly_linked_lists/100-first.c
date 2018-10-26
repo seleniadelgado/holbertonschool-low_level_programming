b010333312b9f7817684e874ed289948b6f83be6
seleniadelgado <angiedelgado08@gmail.com>
2018-10-26 21:10:28 +0000

    100-first.c

---
+++ 0x11-singly_linked_lists/100-first.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+void myStartupFun(void) __attribute__((constructor));
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+}
