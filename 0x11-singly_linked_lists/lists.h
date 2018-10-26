8cbd9344b2fa1ae9b4b98d9b4cbf8c38e21d4ff8
seleniadelgado <angiedelgado08@gmail.com>
2018-10-26 03:11:49 +0000

    adding header with new function

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef _LISTS_H
 #define _LISTS_H
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,4 +25,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
 #endif
