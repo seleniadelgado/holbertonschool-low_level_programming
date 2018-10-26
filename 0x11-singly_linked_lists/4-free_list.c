fe118b0bb2f5a0e0393b66c6510cc1e5a182de28
seleniadelgado <angiedelgado08@gmail.com>
2018-10-26 05:39:15 +0000

    4-free_list.c

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_list - frees a list_t.
+ * @head: begginning of list that needs to be freed.
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		free(head->str);
+	free(head);
+		head=head->next;
+	}
+}
