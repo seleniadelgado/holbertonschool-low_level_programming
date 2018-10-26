e8e0720d33e24715c5623a603a164e15b9ff3875
seleniadelgado <angiedelgado08@gmail.com>
2018-10-26 21:14:42 +0000

    100-first.c

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * myStartupFun - functio that prints before main function is executed. 
+ */
 void myStartupFun(void) __attribute__((constructor));
 void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
