311d2099640badbcf88fadf779d7d33b9f260248
seleniadelgado <angiedelgado08@gmail.com>
2018-10-31 01:03:17 +0000

    1-listint_len.c

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -5,7 +5,7 @@
  * listint_len - function that returns the number of elements in a linked
  * listint_t list.
  * @h: pointer used to iterate through the number of elements in a list.
- * Return : number of elements in a linked list.
+ * Return: number of elements in a linked list.
  */
 size_t listint_len(const listint_t *h)
 {
