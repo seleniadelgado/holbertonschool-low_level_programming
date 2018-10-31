c9b30bac71ba0e40e67de205c58c565e56ce3d20
seleniadelgado <angiedelgado08@gmail.com>
2018-10-31 01:04:49 +0000

    2-add_nodeint.c

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -4,11 +4,13 @@
 /**
  * add_nodeint - function that adds a new node at the beginning of a listint_t
  * @n: parameter used for filling in newhead.
- * @head: head that was removed. 
+ * @head: head that was removed.
+ * Return: new node.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newhead;
+
 	if (head == NULL)
 		return (NULL);
 	newhead = malloc(sizeof(listint_t));
