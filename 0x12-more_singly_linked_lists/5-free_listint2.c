e53d66cf97c9d909a0e4f0a3072c1aa2e9344103
seleniadelgado <angiedelgado08@gmail.com>
2018-10-31 01:15:40 +0000

    5-free_listint2.c

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 /**
  * free_listint2 - function that frees a listint_t list.
  * @head: parameter that needs freeing.
@@ -15,8 +15,8 @@ void free_listint2(listint_t **head)
 		return;
 	while (*head != NULL)
 	{
+		*head = (*head)->next;
 		tmp = *head;
-		tmp = (*head)->next;
 		free(tmp);
 	}
 	head = NULL;
