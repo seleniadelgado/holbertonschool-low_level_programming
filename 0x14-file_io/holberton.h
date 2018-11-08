4f17fc2e29ed66d0330f06c17731c8666bc8ec35
--get <angiedelgado08@gmail.com>
2018-11-08 02:50:03 +0000

    adding new function

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -1,6 +1,7 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
-#include  <stdio.h>
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,4 +11,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv []);
 #endif
