61ac9809421dc9eb1d90fc868d06eeb3713d095f
--get <angiedelgado08@gmail.com>
2018-11-08 02:48:32 +0000

    3-cp.c

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -1,60 +1,77 @@
 #include "holberton.h"
 /**
- * copy_file - function that copies the content of a file to another file.
+ * main - function that copies the content of a file to another file.
  * @argc: parameter used for arguments.
  * @argv: arguments passed.
  * Return: copied file.
  */
 int main(int argc, char *argv[])
 {
-	int file_from;
-	int file_to;
+	int file_from, file_to, runner, text, lamp, garage, door;
 	char pbuffer[1024];
-	int text;
-	int door;
-	int lamp;
-	int garage;
 
 	if (argc != 3)
 	{
-		dprintf("Usage: cp file_from file_to");
+		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to");
 			exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-	{
-		dprintf("Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	text = read(file_from, pbuffer, 1024];
+		openfail(file_from
+	for (runner = 0; pbuffer[runner] != '\0'; runner++)
+		;
+	text = read(file_from, pbuffer, 1024);
 	if (text == -1)
-	{
-		dprintf("Error: Can't read from file %s\n", argv[1]);
-		exit (98);
-	}
+		readfail(text);
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf("Error: Can't write to %s\n", argv[2]);
-		exit (98);
+		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(98);
 	}
-	lamp = write(file_to, pbuffer, 1024);
+	lamp = write(file_to, pbuffer, text);
 	if (lamp == -1)
-	{
-		dprintf("Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+		writefail(lamp)
 	garage = close(file_to);
 	if (garage == -1)
 	{
-		dprintf("Error: Can't close fd %i\n", garage);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %i\n", garage);
 		exit(100);
 	}
 	door = close(file_from);
-	if (door == -1);
+	if (door == -1)
 	{
-		dprintf("Error: Can't close fd %i\n", door);
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %i\n", door);
 		exit(100);
 	}
 	return (0);
+}
+/**
+ * openfail - prints string and exits if open fails.
+ * @s: parameter of type char passed to the function for printing.
+ */
+void openfail(char *s)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't read");
+	dprintf(STDOUT_FILENO, "from file %s\n", argv[1]);
+	exit(98);
+}
+/**
+ * readfail - prints string and exits if read fails.
+ * @s: parameter of type char passed to the function for printing.
+ */
+void readfail(char *s)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't read");
+	dprintf(STDOUT_FILENO, "from file %s\n", argv[1]);
+	exit(98);
+}
+/**
+ * writefail - prints string and exits if write fails.
+ * @s: parameter of type char passed to the function.
+ */
+void writefail(char *s)
+{
+	dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+	exit(99);
 }
