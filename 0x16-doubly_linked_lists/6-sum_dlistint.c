8f7650c6eaa5fe14c5f304d47274cf3d1ca75901
--get <angiedelgado08@gmail.com>
2018-12-08 23:32:29 +0000

    6-sum_dlistint.c

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,9 +1,21 @@
+#include "lists.h"
 /**
- * dlistint_t function that returns the sum of all the data of a linked list.
+ * sum_dlistint - function that returns the sum of all the data of a linked
+ * list.
+ * @head: list that is passed through the function.
  * Return: 0, if empty.
- *
  */
 int sum_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp = head;
+	int sum = 0;
 
+	if (temp == NULL)
+		return (0);
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
 }
