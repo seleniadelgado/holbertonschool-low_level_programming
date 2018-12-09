90f04935388d1f4a555c9cf292170d15d04ef77e
--get <angiedelgado08@gmail.com>
2018-12-09 04:48:08 +0000

    7-insert_dnodeint.c

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -50,7 +50,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 		}
-	if (i + 1)
+	if ((i + 1 ) == idx)
 	{
 		newnode->prev = temp;
 		newnode->next = NULL;
