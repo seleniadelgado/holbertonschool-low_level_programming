4d6a79a45b854b2e7601b71f8856fe2e339cf356
--get <angiedelgado08@gmail.com>
2018-12-09 04:00:38 +0000

    7-insert_dnodeint.c

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,10 +1,50 @@
+#include "lists.h"
 /**
- * insert_dnodeint_at_index - function that inserts a new node at given position
+ * insert_dnodeint_at_index - inserts a new node at given position.
  * @h: list passed through.
  * @idx: index of the list where the new node should be added.
  * @n: node to be inserted.
+ * Return: address of new node.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *newnode, *storage, *temp = *h;
+	unsigned int i = 0;
 
+	if (temp != NULL && idx == 0)
+		return (NULL);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	if (idx == 0)
+	{
+		if (temp != NULL)
+		{
+		newnode->next = temp;
+		temp->prev = newnode;
+		newnode->prev = NULL;
+		*h = newnode;
+		}
+	}
+	while (temp->next != NULL)
+		{
+			if (i == idx)
+			{
+				storage = temp->prev;
+				newnode->prev = storage;
+				newnode->next = temp;
+				storage->next = newnode;
+				temp->prev = newnode;
+				return (newnode);
+			}
+		temp = temp->next;
+		i++;
+		}
+	if (i < idx)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	return (NULL);
 }
