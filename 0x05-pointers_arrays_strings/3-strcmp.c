b05ceacd2058f3ef8d5e3fbf0d1ee99dbdddf377
--get <angiedelgado08@gmail.com>
2018-12-29 00:36:01 +0000

    3-strcmp.c

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a;
-	int b;
+	int a = 0;
+	int b = 0;
 
-	a = 0;
-	b = 0;
 	while (s1[a] != '\0' && s2[b] != '\0')
 	{
-	{
-		if (s1[a] != s2[b])
-		return (s1[a] - s2[b]);
-	}
+		{
+			if (s1[a] != s2[b])
+				return (s1[a] - s2[b]);
+		}
 		a++;
 		b++;
 	}
