c02ef2ef88e2490748942d8e449e88b2a1e01753
--get <angiedelgado08@gmail.com>
2019-01-16 07:07:34 +0000

    6-hash_table_delete.c

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -2,7 +2,33 @@
 /**
  * hash_table_delete - delete a hash table.
  * @ht: is the hashtable.
- *
- *
  */
 void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int hash_number = 0;
+	hash_node_t *object;
+	hash_node_t *temp = NULL;
+
+	if (ht == NULL)
+		return;
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return;
+	}
+	for (hash_number = 0; hash_number < ht->size; hash_number++);
+	{
+		object = ht->array[hash_number];
+		while(object != NULL)
+		{
+			free(object->value);
+			free(object->key);
+			temp = object->next;
+			free(object);
+		}
+		temp = object;
+		free(temp);
+	}
+	free(ht->array);
+	free(ht);
+}
