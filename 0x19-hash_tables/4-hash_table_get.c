76b06f14b1aee0f3a05ebbeead254d9ea6f78c04
--get <angiedelgado08@gmail.com>
2019-01-16 02:40:17 +0000

    4-hash_table_get.c

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 /**
+ * hash_table_get - retrieves a value associated with a key.
  * @ht: is the hash table you want to look into.
  * @key: is the key you are looking for.
  * Return: the value associated with the element, or NULL if key could not be
@@ -7,6 +8,22 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int hash_number;
+	hash_node_t *index;
 
-
+	if (key == NULL)
+		return (NULL);
+	if (ht == NULL)
+		return (NULL);
+	if (strcmp(key, "") == 0)
+		return (NULL);
+	hash_number = key_index((const unsigned char *)key, ht->size);
+	index = ht->array[hash_number];
+	while (index)
+	{
+		if (strcmp(key, index->key) == 0)
+			return (index->value);
+		index = index->next;
+	}
+	return (NULL);
 }
