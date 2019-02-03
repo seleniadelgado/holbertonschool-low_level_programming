5a8a05566f0789251f7badf222747c1550f277aa
--get <angiedelgado08@gmail.com>
2019-02-03 03:06:21 +0000

    adding all the work:

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the Bubble
+ * sort algorithm.
+ * @array: array being sorted.
+ * @size: Number of elements in an array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+        size_t j = 0;
+        int t = 0;
+	size_t len = size - 1;
+	int n = 5;
+
+	if (array == NULL)
+		return;
+	while (j < len)
+	{
+		if (array[j] > array [j+1])
+		{
+			t = array[j];
+			array[j] = array[j+1];
+			array[j+1] = t;
+			print_array(array, size);
+			n = 4;
+		}
+	j++;
+		if (j == len)
+		{
+			if (n == 5)
+				return;
+			else
+			{
+				j = 0;
+				len--;
+				n = 5;
+			}
+		}
+	}
+}
---
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a dlist of ints in ascending order.
+ * list - list to be sorted.
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+
+
+}
