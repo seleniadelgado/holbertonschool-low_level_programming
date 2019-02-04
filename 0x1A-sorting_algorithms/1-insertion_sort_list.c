0530cd2a44c585e03804a7931203477307c3db0a
--get <angiedelgado08@gmail.com>
2019-02-04 23:40:11 +0000

    1-insertion_sort_list.c

--- 0x1A-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1A-sorting_algorithms/1-insertion_sort_list.c
@@ -1,53 +1,47 @@
 #include "sort.h"
 /**
  * insertion_sort_list - sorts a dlist of ints in ascending order.
- * list - list to be sorted.
+ * @list:  list to be sorted.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i = *list;
-	listint_t *y;
-	listint_t *swap;
-	listint_t *rn;
-	listint_t *swap2;
-	listint_t *temp;
+	listint_t *i = *list, *y, *swap, *rn, *swap2, *temp;
+	int a;
 
-	if (list == NULL || (*list)->next == NULL)
+	if ((list == NULL || (*list) == NULL || (*list)->next == NULL))
 		return;
 	while (i->next != NULL)
 	{
 		y = i->next;
+		swap = i;
+		a = 1;
 		if (y->n < i->n)
 		{
-			if (i->prev == NULL)
-			{
-				swap = y;
-				y = i->prev;
-				i->prev = swap;
-				y->prev = NULL;
-				i->next = y->next;
-				y->next = i;
-				*list = y;
-				print_list(*list);
-				i = i->next;
-				continue;
-			}
 			rn = i;
 			swap2 = rn->next;
 			while (rn != NULL)
 			{
-				if (swap2->n > rn->n)
+				if (swap2->n < rn->n)
 				{
 					temp = swap2;
+					rn->next = temp->next;
 					swap2->prev = rn->prev;
+					if (swap->next != NULL)
+						swap2->next->prev = rn;
 					swap2->next = rn;
-					rn->next = swap->next;
+					if (rn->prev != NULL)
+						rn->prev->next = temp;
+					else if (rn->prev == NULL)
+						*list = temp;
 					rn->prev = temp;
 					print_list(*list);
+					if (a == 1)
+					{
+						i = rn;
+						a = 5; }
 				}
-			rn = swap->prev;
-			}
-		}
-	i = i->next;
-	}
-}
+				else
+					break;
+				rn = swap2->prev; } }
+		if (a == 1)
+			i = i->next; } }
