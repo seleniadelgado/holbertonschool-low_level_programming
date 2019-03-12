bb960139279ff825eb129118de139709225ea40e
seleniadelgado <angiedelgado08@gmail.com>
2019-03-12 00:52:46 +0000

    task 0, readme and header

---
+++ 0x1C-binary_trees/0-binary_tree_node.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_node - function that creates a binary code. 
+ * @parent - parent node.
+ * @value - value to be added to the new node.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+
+}
