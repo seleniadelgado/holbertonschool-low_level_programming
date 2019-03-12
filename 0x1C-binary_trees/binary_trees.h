1007090c6b6c6be4d18542d880544567d48ec47b
seleniadelgado <angiedelgado08@gmail.com>
2019-03-12 01:57:25 +0000

    0-binary_tree_node.c

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -1,5 +1,10 @@
 #ifndef _BINARY_TREES_
 #define _BINARY_TREES_
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -39,4 +44,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_print(const binary_tree_t *);
 #endif
