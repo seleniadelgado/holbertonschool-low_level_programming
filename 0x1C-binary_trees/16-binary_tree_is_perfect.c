3c8fc9762835de9eeae743dc189900beb9af3a0f
seleniadelgado <angiedelgado08@gmail.com>
2019-03-14 05:37:05 +0000

    Betty compliant; NULL check

--- 0x1C-binary_trees/16-binary_tree_is_perfect.c
+++ 0x1C-binary_trees/16-binary_tree_is_perfect.c
@@ -6,21 +6,23 @@
  * Return: 1 if it is a perfect binary tree, else 0
  */
 
- int binary_tree_is_perfect(const binary_tree_t *tree)
- {
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
 	int left_height = 0;
-	int right_height= 0;
+	int right_height = 0;
 
+	if (tree == NULL)
+		return (0);
 	if (tree->left)
 		left_height = binary_tree_is_perfect(tree->left) + 1;
 	if (tree->right)
 		right_height = binary_tree_is_perfect(tree->right) + 1;
-	if (!tree->parent)	
+	if (!tree->parent)
 	{
-			if (left_height == 1 ||  right_height == 1)
-				return (0);
-			else
-				return (1);
+		if (left_height == 1 ||  right_height == 1)
+			return (0);
+		else
+			return (1);
 	}
 	if (left_height != right_height)
 		return (0);
@@ -30,4 +32,4 @@
 	if (!tree->left || !tree->right)
 		return (0);
 	return (left_height);
- }
+}
