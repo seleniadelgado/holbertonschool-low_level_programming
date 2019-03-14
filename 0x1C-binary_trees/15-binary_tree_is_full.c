1cf770ae882615c267ea4680f7375f024b5f32e7
seleniadelgado <angiedelgado08@gmail.com>
2019-03-14 00:56:51 +0000

    checks for full tree

---
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: The root of the tree
+ * Return: 1 if it is a full binary tree, else 0
+ */
+
+ int binary_tree_is_full(const binary_tree_t *tree)
+ {
+	// itertate through each node and see if any of them have only 1 child
+	// if it does, then return 0
+	// else return 1
+	int full_flag = 1;
+
+	if (tree->left)
+		full_flag = binary_tree_is_full(tree->left);
+	if (tree->right)
+		full_flag = binary_tree_is_full(tree->right);
+
+	if (tree->left && !tree->right)
+		return (0);
+	if (!tree->left && tree->right)
+		return (0);
+	return (full_flag);
+ }
