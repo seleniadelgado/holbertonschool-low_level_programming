3c432a9adce7b8ae1672d17d4e9f281ab14ab978
seleniadelgado <angiedelgado08@gmail.com>
2019-03-14 01:17:53 +0000

    added a plus one to the right side

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -8,9 +8,6 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	// get the height of the left tree
-	// get the height of the right tree
-	// get the difference between the height of the left and right tree
 	int balance_left = 0;
 	int balance_right = 0;
 	int balance_factor = 0;
@@ -18,7 +15,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree->left)
 		balance_left += binary_tree_balance(tree->left) + 1;
 	if (tree->right)
-		balance_right += binary_tree_balance(tree->right) + 0;
+		balance_right += binary_tree_balance(tree->right) + 1;
 
 	balance_factor = balance_left - balance_right;	
 	return(balance_factor);
