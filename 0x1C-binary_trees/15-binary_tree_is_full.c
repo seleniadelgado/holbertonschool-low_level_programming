0e5dc787f40c1a4bdb0bb61cf81efa06c13cfa5e
seleniadelgado <angiedelgado08@gmail.com>
2019-03-14 03:04:32 +0000

    null check

--- 0x1C-binary_trees/15-binary_tree_is_full.c
+++ 0x1C-binary_trees/15-binary_tree_is_full.c
@@ -6,10 +6,12 @@
  * Return: 1 if it is a full binary tree, else 0
  */
 
- int binary_tree_is_full(const binary_tree_t *tree)
- {
+int binary_tree_is_full(const binary_tree_t *tree)
+{
 	int full_flag = 1;
 
+	if (tree == NULL)
+		return (0);
 	if (tree->left)
 		full_flag = binary_tree_is_full(tree->left);
 	if (tree->right)
@@ -20,4 +22,4 @@
 	if (!tree->left && tree->right)
 		return (0);
 	return (full_flag);
- }
+}
