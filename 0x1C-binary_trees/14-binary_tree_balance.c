c0d01e803134d8c77dfdf0b781573edfd3b7f8e5
seleniadelgado <angiedelgado08@gmail.com>
2019-03-14 01:23:15 +0000

    checking for null

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -12,11 +12,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int balance_right = 0;
 	int balance_factor = 0;
 
+	if (tree == NULL)
+	  return (0);
 	if (tree->left)
 		balance_left += binary_tree_balance(tree->left) + 1;
 	if (tree->right)
 		balance_right += binary_tree_balance(tree->right) + 1;
-
-	balance_factor = balance_left - balance_right;	
-	return(balance_factor);
+	balance_factor = balance_left - balance_right;
+	return (balance_factor);
 }
