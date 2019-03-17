0920828d228d4b0e6412560fd4a46829a10c9f56
stak21 <stakashima21@gmail.com>
2019-03-17 09:28:27 +0000

    Added new prototype binary_tree_rotate_left

--- 0x1C-binary_trees/binary_trees.h
+++ 0x1C-binary_trees/binary_trees.h
@@ -65,6 +65,7 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 bst_t *binary_trees_ancestor(const bst_t *f, const bst_t *);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 void binary_tree_levelorder(const bst_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
 #endif
