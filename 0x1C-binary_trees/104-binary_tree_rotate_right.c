00fd6ae606331ca6219148033600cf9f0faffe7a
stak21 <stakashima21@gmail.com>
2019-03-17 09:42:23 +0000

    Removed extra print statement

--- 0x1C-binary_trees/104-binary_tree_rotate_right.c
+++ 0x1C-binary_trees/104-binary_tree_rotate_right.c
@@ -10,7 +10,6 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *temp = NULL;
 	binary_tree_t *new_root = NULL;
-		printf("what\n");
 
 	if (!tree)
 		return (NULL);
