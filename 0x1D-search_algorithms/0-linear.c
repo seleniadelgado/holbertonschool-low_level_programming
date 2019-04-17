1c75cc1b77724512c9b1b74c41edb71a28a32084
--get <angiedelgado08@gmail.com>
2019-04-17 23:08:07 +0000

    0-linear.c

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -5,17 +5,20 @@
  * @array: pointer to the first element of the array to seach in.
  * @size: number of elements in the array.
  * @value: value to search for in an array
+ * Return: -1 if null or array position
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int p = 0;
+	int p;
+	int a = size;
 
 	if (array == NULL)
 		return (-1);
-	for (array[p]; array[p] < size, array++)
-		if (array == value)
-			return (array[p]);
-		else
-
-
+	for (p = 0; p != a; p++)
+	{
+		printf("Value checked array [%d] = [%d]\n", p, array[p]);
+		if (array[p] == value)
+			return (p);
+	}
+	return (-1);
 }
