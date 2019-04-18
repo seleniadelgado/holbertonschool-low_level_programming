49af714673730f628657c749da613ea92e187ca3
--get <angiedelgado08@gmail.com>
2019-04-18 06:23:20 +0000

    1-binary.c

---
+++ 0x1D-search_algorithms/1-binary.c
@@ -0,0 +1,21 @@
+#include "search.algos.h"
+/**
+ * binary_search - function that searches for a value in a ascending order
+ * sorted integer array using Binary search algorithm.
+ * @array: pointer to the first element in an array.]
+ * @value: the value to search for.
+ * @size: size of the array.
+ * Return: -1 if NULL or array position.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	if (size == 0)
+		return (-1);
+	res = binary_search(array, 1, val)
+
+
+
+
+}
